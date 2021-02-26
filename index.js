ee84ebd993f36c14aeb97b76057a368a32ab8f31
Yuya Kasai <moyoyomiyazawa@gmail.com>
2021-02-26 19:54:48 +0900

    Prettierで整形

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const sid = process.env.SID;
 
 const options = {
   headers: {
-    Cookie: `connect.sid=${sid}`
+    Cookie: sid ? `connect.sid=${sid}` : '',
   },
   json: true,
-  maxAge: 60000
+  maxAge: 60000,
 };
 
 const createItem = (title, subtitle, url) => {
@@ -17,32 +17,51 @@ const createItem = (title, subtitle, url) => {
     subtitle,
     arg: url,
     icon: {
-      type: "png",
-      path: "icon.png",
-    }
+      type: 'png',
+      path: 'icon.png',
+    },
   };
-}
+};
 
 (async () => {
-  if(!projectName || !sid) {
-    alfy.output([createItem("Please set environment variable. ⚠️", "Set API_TOKEN and USER_NAME to workflow environment variable", "")])
+  if (!projectName) {
+    alfy.output([
+      createItem(
+        'Please set environment variable. ⚠️',
+        'Set PROJECT_NAME to workflow environment variable',
+        ''
+      ),
+    ]);
   }
 
-  if(alfy.input.length > 1) {
+  if (alfy.input.length > 1) {
     const posts = await alfy.fetch(
       `https://scrapbox.io/api/pages/${projectName}?limit=1000`,
       options
     );
 
-    const items = alfy.inputMatches(posts.pages, 'title').map(p => createItem(p.title, p.descriptions[0], `https://scrapbox.io/${projectName}/${p.title}`));
+    const items = alfy
+      .inputMatches(posts.pages, 'title')
+      .map((p) =>
+        createItem(
+          p.title,
+          p.descriptions[0],
+          `https://scrapbox.io/${projectName}/${p.title}`
+        )
+      );
     if (!items.length) {
-      alfy.output(
-          [createItem('The requested post was not found.', 'Do a full-text search on the Scrapbox.', `https://scrapbox.io/${projectName}/search/page?q=${alfy.input}`)]);
+      alfy.output([
+        createItem(
+          'The requested post was not found.',
+          'Do a full-text search on the Scrapbox.',
+          `https://scrapbox.io/${projectName}/search/page?q=${alfy.input}`
+        ),
+      ]);
       return;
     }
 
     alfy.output(items);
   } else {
-    alfy.output([createItem("Loading...", "", "")])
+    alfy.output([createItem('Loading...', '', '')]);
   }
-})()
+})();
